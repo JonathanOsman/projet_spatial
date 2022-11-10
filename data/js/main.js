bd7ab7afccf862cbc07d3c42817ea04b3fb8dc86
JonathanOsman <osman-2009@hotmail.fr>
2022-11-10 16:19:44 +0100

    fin journée 1

--- data/js/main.js
+++ data/js/main.js
@@ -1,1 +1,22 @@
+//------ Le décompteur de la prochaine mission ------
 
+let deadline = new Date("Nov 14, 2022 06:07:00").getTime();
+
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let t = deadline - now;
+    console.log(t);
+    let days = Math.floor( t / ( 1000 * 60 * 60 * 24 ));
+    let hours = Math.floor(( t% (1000 * 60 * 60 * 24) ) / ( 1000 * 60 * 60 ));
+    let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((t % (1000 * 60)) / 1000);
+
+    document.getElementById("day").innerHTML = days;
+    document.getElementById("hour").innerHTML = hours;
+    document.getElementById("minute").innerHTML = minutes;
+    document.getElementById("second").innerHTML = seconds;
+
+    }
+
+)
+//------ Le décompteur de la prochaine mission (FIN DE SCRIPT)------
